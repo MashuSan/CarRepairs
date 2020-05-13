442ce030fe8ecef0963aebb22a7b5b3b427786fb
Marek Kadlečík <xkadlec5@fi.muni.cz>
2020-05-13 17:22:38 +0200

    Record details

--- src/frontend-sbook/src/services/downloadRecordData.js
+++ src/frontend-sbook/src/services/downloadRecordData.js
@@ -1,5 +1,5 @@
 export async function downloadRecord(id) {
     const eventData = await fetch("http://localhost:5000/services/" + id);
     if (!eventData){return 0;}
-    return eventData;
+    return await eventData.json();
 }
